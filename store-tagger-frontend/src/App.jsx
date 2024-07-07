5747550905b8acffd8029455d8128a10774c8070
Edison Caceres Alejo <edisonyca127@gmail.com>
2024-07-06 22:53:04 -0400

    Added code to fetch Flask and display endpoint. Code is not functioning.

--- store-tagger-frontend/src/App.jsx
+++ store-tagger-frontend/src/App.jsx
@@ -1,11 +1,42 @@
 import "../index.css";
 import Add from "./components/Add";
+import { useState } from "react";
 
 function App() {
+  // usestate for setting a javascript
+  // object for storing and using data
+  const [data, setdata] = useState({
+    name: "",
+    age: 0,
+    date: "",
+    programming: "",
+  });
+
+  // Using useEffect for single rendering
+  useEffect(() => {
+    // Using fetch to fetch the api from
+    // flask server it will be redirected to proxy
+    fetch("/data").then((res) =>
+      res.json().then((data) => {
+        // Setting a data from api
+        setdata({
+          name: data.Name,
+          age: data.Age,
+          date: data.Date,
+          programming: data.programming,
+        });
+      })
+    );
+  }, []);
+
   return (
     <>
       <body>
         <Add add_type={"Store"}></Add>
+        <p>{data.name}</p>
+        <p>{data.age}</p>
+        <p>{data.date}</p>
+        <p>{data.programming}</p>
       </body>
     </>
   );
